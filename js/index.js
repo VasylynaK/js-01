25723dd89a5147bb77954496cf9163beec10738b
Vasylyna Katsma <v.katsma@gmail.com>
2017-04-03 15:18:19 +0000

    change

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var name = prompt("What is your name?");
 var greating = "Hello " + name +'.' + " I will help you solve the quadratic equation";
 document.write(greating);
 
-function squareRoot (a,b,c) {
+function squareEquation (a,b,c) {
     var discriminant = b*b - 4*a*c;
     var x1 = (-b + Math.sqrt(discriminant))/(2*a);
     var x2 = (-b - Math.sqrt(discriminant))/(2*a);
@@ -12,7 +12,7 @@ function squareRoot (a,b,c) {
 var a = Number(prompt('Please input parameter a'));
 var b= Number(prompt('Please input parameter b'));
 var c = Number(prompt('Please input parameter c'));
-var calc = squareRoot (a, b, c);
+var calc = squareEquation (a, b, c);
 var result = "X1= " + calc[0] + "; X2= " + calc[1];
 var userInput = a + "x<sup>2</sup> +" +  b  + "x +" + c + "=0"; 
 document.write("<br>" + "Your equation: " + userInput);
